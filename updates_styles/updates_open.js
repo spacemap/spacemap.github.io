30b949998980ef8e0fbb4bda948e2de49f4af3ca
spacemap <31139924+spacemap@users.noreply.github.com>
2017-08-31 20:50:47 +0300

    Add files via upload

--- updates_styles/updates_open.js
+++ updates_styles/updates_open.js
@@ -1,5 +1,12 @@
 ﻿
+var openWindow=0;
+
 $('#patch_624').click(function(){
+
+	if (openWindow==624){
+		openWindow=0;
+	$('div#content_624').fadeOut(0);
+	} else {
 $('div#content_436').fadeOut(0);
 $('div#content_437').fadeOut(0);
 $('div#content_439').fadeOut(0);
@@ -10,9 +17,17 @@ $('div#content_510').fadeOut(0);
 $('div#content_620').fadeOut(0);
 $('div#content_624').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Усовершентсвована работа вывода горизонтальных координат<li>Увеличена база данных<li>Добавлен расширенный поиск</ul></div></td></table>")
 $('div#content_624').fadeIn(0);
+
+	openWindow=624;
+
+	}
 });
 
 $('#patch_620').click(function(){
+		if (openWindow==620){
+		openWindow=0;
+	$('div#content_620').fadeOut(0);
+	} else {
 $('div#content_436').fadeOut(0);
 $('div#content_437').fadeOut(0);
 $('div#content_439').fadeOut(0);
@@ -23,9 +38,17 @@ $('div#content_510').fadeOut(0);
 $('div#content_624').fadeOut(0);
 $('div#content_620').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Добавлена Анимация в раздел 'Солнечная система'<li>Введены горизонтальные координаты<ul></div></td></table>")
 $('div#content_620').fadeIn(0);
+
+	openWindow=620;
+
+	}
 });
 
 $('#patch_510').click(function(){
+		if (openWindow==510){
+		openWindow=0;
+	$('div#content_510').fadeOut(0);
+	} else {
 $('div#content_436').fadeOut(0);
 $('div#content_437').fadeOut(0);
 $('div#content_439').fadeOut(0);
@@ -36,9 +59,17 @@ $('div#content_620').fadeOut(0);
 $('div#content_624').fadeOut(0);
 $('div#content_510').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Теперь орбиты планет почти пропорциональны настоящим<li>Решена проблема с очень маленькими орбитами/планетами земной группы<ul></div></td></table>")
 $('div#content_510').fadeIn(0);
+
+	openWindow=510;
+
+	}
 });
 
 $('#patch_500').click(function(){
+		if (openWindow==500){
+		openWindow=0;
+	$('div#content_500').fadeOut(0);
+	} else {
 $('div#content_436').fadeOut(0);
 $('div#content_437').fadeOut(0);
 $('div#content_439').fadeOut(0);
@@ -49,9 +80,17 @@ $('div#content_620').fadeOut(0);
 $('div#content_624').fadeOut(0);
 $('div#content_500').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Лупа исправлена еще более целиком<li>Редизайн раздела звезды<li>Добавлен новый раздел - Солнечная система<li>Переработан первичный интерфейс программы<li>Все элементы memo переведены в режим ReadOnly<li>База данных теперь общая<li>Исправлен баг с ошибочнвм поиском созвездий<li>Создан новый алгоритм работы кнопки 'создатели'<li>Локальное небо теперь работает с текущей датой<li>Мэйн кнопка тоже работает с датой<ul></div></td></table>")
 $('div#content_500').fadeIn(0);
+
+	openWindow=500;
+
+	}
 });
 
 $('#patch_443').click(function(){
+		if (openWindow==443){
+		openWindow=0;
+	$('div#content_443').fadeOut(0);
+	} else {
 $('div#content_436').fadeOut(0);
 $('div#content_437').fadeOut(0);
 $('div#content_439').fadeOut(0);
@@ -62,9 +101,17 @@ $('div#content_620').fadeOut(0);
 $('div#content_624').fadeOut(0);
 $('div#content_443').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Лупа исправлена целиком<ul></div></td></table>")
 $('div#content_443').fadeIn(0);
+
+	openWindow=443;
+
+	}
 });
 
 $('#patch_440').click(function(){
+		if (openWindow==440){
+		openWindow=0;
+	$('div#content_440').fadeOut(0);
+	} else {
 $('div#content_436').fadeOut(0);
 $('div#content_437').fadeOut(0);
 $('div#content_439').fadeOut(0);
@@ -75,9 +122,17 @@ $('div#content_620').fadeOut(0);
 $('div#content_624').fadeOut(0);
 $('div#content_440').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Добавлена нормальная работа с созвездиями<li>Добавлен более крутой вывод при поиске звезды<ul></div></td></table>")
 $('div#content_440').fadeIn(0);
+
+	openWindow=440;
+
+	}
 });
 
 $('#patch_439').click(function(){
+		if (openWindow==439){
+		openWindow=0;
+	$('div#content_439').fadeOut(0);
+	} else {
 $('div#content_436').fadeOut(0);
 $('div#content_437').fadeOut(0);
 $('div#content_440').fadeOut(0);
@@ -87,10 +142,17 @@ $('div#content_510').fadeOut(0);
 $('div#content_620').fadeOut(0);
 $('div#content_624').fadeOut(0);
 $('div#content_439').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Пофикшены формы выводимых на экран фигур<ul></div></td></table>")
-$('div#content_439').fadeIn(100);
+$('div#content_439').fadeIn(0);
+	openWindow=439;
+
+	}
 });
 
 $('#patch_437').click(function(){
+		if (openWindow==437){
+		openWindow=0;
+	$('div#content_437').fadeOut(0);
+	} else {
 $('div#content_436').fadeOut(0);
 $('div#content_439').fadeOut(0);
 $('div#content_440').fadeOut(0);
@@ -100,10 +162,18 @@ $('div#content_510').fadeOut(0);
 $('div#content_620').fadeOut(0);
 $('div#content_624').fadeOut(0);
 $('div#content_437').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Линия лупы изменена на пунктир<li>Прямоугольник увеличения больше не красит стороны<li>Исправлен баг, при котором не исчезала кнопка 'вернуться к карте'<ul></div></td></table>")
-$('div#content_437').fadeIn(100);
+$('div#content_437').fadeIn(0);
+
+	openWindow=437;
+
+	}
 });
 
 $('#patch_436').click(function(){
+		if (openWindow==436){
+		openWindow=0;
+	$('div#content_436').fadeOut(0);
+	} else {
 $('div#content_437').fadeOut(0);
 $('div#content_439').fadeOut(0);
 $('div#content_440').fadeOut(0);
@@ -114,4 +184,8 @@ $('div#content_620').fadeOut(0);
 $('div#content_624').fadeOut(0);
 $('div#content_436').html("<table class='patch_content'><td class='patch_content_td'><div class='patch_content_text'><ul><li>Исправлен баг с отображением размера при поиске<li>Исправлен баг с none'ом<li>Добавлены две строки про вопросы в эклиптику/найти созвездие/как работает<li>Исправлен баг с функцией левенштейна<li>Исправлен баг с отображением имени созвездия при опечатке<ul></div></td></table>")
 $('div#content_436').fadeIn(0);
+
+	openWindow=436;
+
+	}
 });
